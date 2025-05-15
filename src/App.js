575c098340949c09e3a56f345a36dfc7427eb93b
shivank-coder <shivankjh02@gmail.com>
2025-05-15 17:02:29 +0000

    vanila component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+
+const pizza=(props)=>{
+    return React.createElement(
+        "div",
+        {},
+        React.createElement("h1",{},props.name),
+        React.createElement("p",{},props.desciption),
+        React.createElement("p",{},props.type)
+
+
+    );
+}
+
+
 const App=() =>
 {
     return React.createElement(
         "div",
         {},
-        React.createElement("h1",{},"padro's")
+        React.createElement("h1",{},"padro's"),
+        React.createElement(pizza,{name:"capiscum"}),
+     React.createElement(pizza,{desciption:"its saltey in nature with some extra gravy" }),
+          React.createElement(pizza,{type:"basic pizza" }),
+
+
+
 
         );
     
